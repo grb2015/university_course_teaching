b856199746afe628067fe63baca681dc5336163f
guorenbin <XMG3-Rel.1>
2019-03-17 17:52:52 +0800

    modify

--- newYorkUniversity_os/cpu_scheduling/Scheduling-master/back_Scheduler_all_algorithm.java
+++ newYorkUniversity_os/cpu_scheduling/Scheduling-master/back_Scheduler_all_algorithm.java
@@ -167,7 +167,7 @@ public class Scheduler {
 				if (cpuBT == 0){		// 如果剩余时间为0了
 					Process temp = runTable.get(0);		// 则从就绪队列中取新的进来
 					if (temp.getC() > 0){		// 如果取得这个新的剩余时间是大于0的
-						temp.setState(3);		// 则阻塞这个进程.应该进入CPU burst了。所以需要阻塞
+						temp.setState(3);		// 则阻塞这个进程?!! Q2
 						int temp1 = randomOS(temp.getIO());
 						// System.out.print("frunList.indexOf(temp) = " + runList.indexOf(temp)+"\n");
 						blocktime[runList.indexOf(temp)] = temp1;  // 设置该进程的阻塞时间为temp1
@@ -240,8 +240,7 @@ public class Scheduler {
 		System.out.println("\t" + "Average Wait time: " + avgWait);
 		System.out.println();
 	}
-
-
+	
 	public void RR(){
 		int time = 0,count = 0,cpuBT = 0, quantum = 2;
 		double cpuutil = 0;	 	
@@ -254,19 +253,10 @@ public class Scheduler {
 		Process running = null;
 		ArrayList<Process> readyTable = new ArrayList<Process>();
 		blocktime = new int[sList.size()];
-		cputime = new int[sList.size()];	// Q1 ： CPU time是干啥的? 它用户存放cpu当前的burst中能运行的时间
-											// 之前的FCFS中,cpu 的一个burst一定可以运行完，不会被中断。但是现在不一定了
-											// 也即是说,这里的cputime保存的是一个cpu burst的剩余时间。
-											// 比如这个cpu burst 为8 但是quantum为2,运行完一个 quantum 后
-											// cputime 就变为了6 了。只有当cputime变成0后,当前Process才需要
-											// 去获取下一个CPU burst
-		for (int i = 0; i < sList.size(); i++){		
+		cputime = new int[sList.size()];
+		for (int i = 0; i < sList.size(); i++){
 			blocktime[i] = -1;
 			cputime[i] = 0;
-		}
-		System.out.print("cputime = ");
-		for (int i = 0; i < cputime.size(); i++){
-			System.out.print(cputime[i]);
 		}
 		if (verbose){
 			System.out.print("Before Cycle    " + time + ":");
@@ -276,17 +266,13 @@ public class Scheduler {
 		}
 		System.out.print("\n");
 		while (check){
-			// 遍历阻塞队列,看是否有队列等待时间到了,到了则加入到就绪队列。没到什么事情都不做。
-			// 下面也有遍历阻塞队列的地方。放在一起不是更好? Q3
 			for (int i = 0; i < blocktime.length; i++){
 				if (blocktime[i] == 0){
 					readyTable.add(runList.get(i));
-					blocktime[i] = -1;// 设置当前进程阻塞时间为-1 (非阻塞)
-					runList.get(i).setState(1);	// 设置当前进程状态为就绪
+					blocktime[i] = -1;
+					runList.get(i).setState(1);
 				}
 			}
-			// 遍历队列,查看是否有新进程创建了。(新创建的进程会直接加入到就绪队列)
-			// 如果进程的到达时间 = 当前时间  ,即有进程来了,则将进程放到就绪队列中
 			for (Process k : runList){
 				if (k.getA() == time){
 					readyTable.add(k);
@@ -294,30 +280,28 @@ public class Scheduler {
 				}
 			}
 			time ++;
-			if (running != null){	// 若当前有Process在运行,CPU运行时间+1
+			if (running != null){
 				running.decreaseC();
 				cpuutil ++;
-			}else if(!readyTable.isEmpty()){	// 如果没有Process在运行,但是就绪队列不为空
+			}else if(!readyTable.isEmpty()){
 				int min = 99999;
 				// the following step choose the earliest arrival process, but the sample output didn't follow this rule
 				Process temp = null;
-				// 从最小的队列中下标最小的的进程,即保证FCFS。进程存放到temp,该进程在runList中的index存放在Min中
-				for (Process p : readyTable){	
+				for (Process p : readyTable){
 					if ((runList.indexOf(p)) < min){
 						temp = p;
 						min = runList.indexOf(p);
 					}
 				}
-				// 从就绪队列中宁取出第一个
-				running = readyTable.get(0);	
-				readyTable.remove(readyTable.indexOf(running));	
-				running.setState(2);// 设置进程为运行中
-				if (cputime[runList.indexOf(running)] == 0){	// 如果该进程的cputime为0
-					int temp1 = randomOS(running.getB());	// 获取该进程下一cpu burst的时间为 temp1
+				running = readyTable.get(0);
+				readyTable.remove(readyTable.indexOf(running));
+				running.setState(2);
+				if (cputime[runList.indexOf(running)] == 0){
+					int temp1 = randomOS(running.getB());
 					if (temp1 > running.getC()){
 						temp1 = running.getC();
 					}
-					cputime[runList.indexOf(running)] = temp1;	// 将cputime设置为 temp1
+					cputime[runList.indexOf(running)] = temp1;
 					if (temp1 > quantum) cpuBT = quantum;
 					else cpuBT = temp1;
 				}else{
@@ -418,7 +402,251 @@ public class Scheduler {
 		System.out.println();
 	}
 	
-
+	public void UNI(){
+		int time = 0,count = 0,cpuBT = 0,blockTime = -1;
+		double cpuutil = 0, ioutil = 0;
+		boolean check = true;
+		ArrayList<Process> runList = new ArrayList<Process>();
+		for(Process k : sList){
+			runList.add(k);
+		}
+		ArrayList<Process> runTable = new ArrayList<Process>();
+		if (verbose){
+			System.out.print("Before Cycle    " + time + ":");
+			for (int i = 0; i < runList.size(); i++){
+				System.out.print("unstarted 0" + "\t");
+			}
+		}
+		System.out.print("\n");
+		while (check){
+			for (Process k : runList){
+				if (k.getA() == time){
+					runTable.add(k);
+					k.setState(1);
+				}
+			}
+			time ++;
+			if (blockTime == 0){
+				blockTime = -1;
+				runTable.get(0).setState(1);
+			}
+			if (!runTable.isEmpty()){
+				Process temp = runTable.get(0);
+				if (temp.getState() == 1){
+					int temp1 = randomOS(temp.getB());
+					if (temp1 < temp.getC()) cpuBT = temp1;
+					else cpuBT = temp.getC();
+					temp.setState(2);
+					temp.decreaseC();
+					cpuutil ++;
+				}else if(temp.getState() == 2){
+					temp.decreaseC();
+					cpuutil ++;
+				}
+			}
+			if (verbose){
+				System.out.print("Before Cycle    " + time + ":");
+				for (Process k: runList){
+					if (k.getState() == 0){
+						System.out.print("unstarted 0" + "\t");
+					}else if(k.getState() == 1){
+						System.out.print("ready     0" + "\t");
+						k.incWT();
+					}else if(k.getState() == 2){
+						System.out.print("running   " + cpuBT + "\t");
+					}else if(k.getState() == 3){
+						System.out.print("blocked   " + blockTime + "\t");
+					}else{
+						System.out.print("finished  0" + "\t");
+					}	
+				}
+				System.out.print("\n");
+			}
+			
+			if (blockTime > 0){
+				blockTime --;
+				ioutil ++;
+				runTable.get(0).incIOT();
+			}
+			if (cpuBT > 0){
+				cpuBT --;
+				if (cpuBT == 0){
+					Process temp = runTable.get(0);
+					if (temp.getC() > 0){
+						temp.setState(3);
+						blockTime = randomOS(temp.getIO());
+					}else{
+						temp.setft(time);
+						temp.setState(4);
+						runTable.remove(0);
+						count++;
+					}
+				}
+			}
+			if (count == runList.size()) check = false;
+		}
+		System.out.println();
+		System.out.println("The algorithm used is Uniprogrammed");
+		System.out.println();
+		double avgWait = 0, avgTurn = 0, max = -1;
+		for(Process p:runList){
+			if (p.getft() > max) max = p.getft();
+			avgTurn += p.getft() - p.getA();
+			avgWait += p.getWT();
+			System.out.println("Process " + runList.indexOf(p) + ":");
+			System.out.println("\t" +"(A,B,C,IO) = (" + p.getA() + "," + p.getB() + "," + p.getintialC() +"," + p.getIO() +")");
+			System.out.println("\t" + "Finishing time: " + p.getft());
+			System.out.println("\t" + "Turnaround time: " + (p.getft() - p.getA()));
+			System.out.println("\t" + "I/O time: " + p.getIOT());
+			System.out.println("\t" + "Waiting time: " + p.getWT());
+		}
+		System.out.println();
+		avgWait = avgWait / runList.size();
+		avgTurn = avgTurn / runList.size();
+		System.out.println("Summary Data: ");
+		System.out.println("\t" + "Finishing time: " + max);
+		System.out.println("\t" + "CPU Utilization: " + cpuutil/max);
+		System.out.println("\t" + "IO Utilization: " + ioutil/max);
+		System.out.println("\t" + "Troughput " + (runList.size() / max) * 100 +" processes per hundread cycles");
+		System.out.println("\t" + "Average Turnaround time: " + avgTurn);
+		System.out.println("\t" + "Average Wait time: " + avgWait);
+		System.out.println();
+	}
+	
+	public void PSJF(){
+		int time = 0,count = 0,cpuBT = 0;
+		double cpuutil = 0, ioutil = 0;
+		boolean check = true;
+		ArrayList<Process> runList = new ArrayList<Process>();
+		for(Process k : sList){
+			runList.add(k);
+		}
+		ArrayList<Process> readyTable = new ArrayList<Process>();
+		Process running = null;
+		blocktime = new int[runList.size()];
+		for (int i = 0; i < runList.size(); i++){
+			blocktime[i] = -1;
+		}
+		if (verbose){
+			System.out.print("Before Cycle    " + time + ":");
+			for (int i = 0; i < runList.size(); i++){
+				System.out.print("unstarted 0" + "\t");
+			}
+		}
+		System.out.print("\n");
+		while(check){
+			for (Process k : runList){
+				if (k.getA() == time){
+					readyTable.add(k);
+					k.setState(1);
+				}
+			}
+			time ++;
+			for (int i = 0; i < blocktime.length; i++){
+				if (blocktime[i] == 0){
+					readyTable.add(runList.get(i));
+					runList.get(i).setState(1);
+					blocktime[i] = -1;
+				}
+			}
+			if (running != null){
+				running.decreaseC();
+				cpuutil ++;
+			}else if(!readyTable.isEmpty()){
+				int min = 99999;
+				Process temp = null;
+				for (Process p : readyTable){
+					if (p.getC() < min){
+						temp = p;
+						min = temp.getC();
+					}
+				}
+				running = temp;
+				readyTable.remove(readyTable.indexOf(running));
+				int temp1 = randomOS(running.getB());  // randomOS(B)
+				if (temp1 < running.getC()) cpuBT = temp1;
+				else cpuBT = running.getC();
+				running.setState(2);
+				running.decreaseC();
+				cpuutil ++;
+			}
+			if (verbose){
+				System.out.print("Before Cycle    " + time + ":");
+				for (Process k: runList){
+					if (k.getState() == 0){
+						System.out.print("unstarted 0" + "\t");
+					}else if(k.getState() == 1){
+						System.out.print("ready     0" + "\t");
+						k.incWT();
+					}else if(k.getState() == 2){
+						System.out.print("running   " + cpuBT + "\t");
+					}else if(k.getState() == 3){
+						System.out.print("blocked   " + blocktime[runList.indexOf(k)] + "\t");
+					}else{
+						System.out.print("finished  0" + "\t");
+					}	
+				}
+				System.out.print("\n");
+			}
+			
+			for (int i = 0; i < blocktime.length; i++){
+				if (blocktime[i] > 0){
+					blocktime[i] --;
+					runList.get(i).incIOT();
+					ioutil ++;
+				}
+			}
+			if (cpuBT > 0){
+				cpuBT--;
+				if (cpuBT == 0){
+					if (running.getC() > 0){
+						running.setState(3);
+						int temp1 = randomOS(running.getIO());
+						blocktime[runList.indexOf(running)] = temp1;
+					}else{
+						running.setft(time);
+						running.setState(4);
+						count ++;
+					}
+					running = null;
+				}
+			}
+			if (count == runList.size()) check = false;
+		}
+		System.out.println();
+		System.out.println("The algorithm used is Preemptive Shortest Job First");
+		System.out.println();
+		double avgWait = 0, avgTurn = 0, max = -1;
+		for(Process p:runList){
+			if (p.getft() > max) max = p.getft();
+			avgTurn += p.getft() - p.getA();
+			avgWait += p.getWT();
+			System.out.println("Process " + runList.indexOf(p) + ":");
+			System.out.println("\t" +"(A,B,C,IO) = (" + p.getA() + "," + p.getB() + "," + p.getintialC() +"," + p.getIO() +")");
+			System.out.println("\t" + "Finishing time: " + p.getft());
+			System.out.println("\t" + "Turnaround time: " + (p.getft() - p.getA()));
+			System.out.println("\t" + "I/O time: " + p.getIOT());
+			System.out.println("\t" + "Waiting time: " + p.getWT());
+		}
+		System.out.println();
+		avgWait = avgWait / runList.size();
+		avgTurn = avgTurn / runList.size();
+		System.out.println("Summary Data: ");
+		// 表明该进程结束的时间,Q1它与avgTurn有什么区别?基本山是等同的。
+		System.out.println("\t" + "Finishing time: " + max);
+		// CPU使用率 : 比如100ms中,CPU使用了70ms,CPU等待IO花了30ms 则CPU使用率为 70%
+		System.out.println("\t" + "CPU Utilization: " + cpuutil/max);
+		// IO使用时间
+		System.out.println("\t" + "IO Utilization: " + ioutil/max);
+		// 吞吐量 = 单位时间内可完成的进程数量(这里是100ms)
+		System.out.println("\t" + "Troughput " + (runList.size() / max) * 100 +" processes per hundread cycles");
+		// 平均周转时间 = 该进程运行了多长时间 (即从进程提交到进程完成的时间段称为周转时间)
+		System.out.println("\t" + "Average Turnaround time: " + avgTurn);
+		// 平均等待时间  ： 在就绪队列中等待所花费的时间
+		System.out.println("\t" + "Average Wait time: " + avgWait);
+		System.out.println();
+	}
+	
 	public static void main(String args[]) throws Exception{
 		Scheduler s = new Scheduler();
 		if (args.length == 2){
