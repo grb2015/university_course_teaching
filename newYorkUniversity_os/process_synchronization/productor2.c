c7b486b23580efcfd5dc0fddb0a04c1c116d71d1
grb2015 <949409706@qq.com>
2019-03-17 20:20:12 +0800

    use sem_t to replace pthread_lock AND remove sleep(1)

--- newYorkUniversity_os/process_synchronization/productor2.c
+++ newYorkUniversity_os/process_synchronization/productor2.c
@@ -1,6 +1,7 @@
 /*
-* author 张文
-* 2008/06/20
+
+uage: gcc productor2.c -o productor2 -lpthread 
+note: you should link pthread !
 */
 
 
@@ -20,8 +21,8 @@ int buff[M] = {0}; // 缓冲初始化为0， 开始时没有产品
 
 sem_t empty_sem; // 同步信号量， 当满了时阻止生产者放产品
 sem_t full_sem;   // 同步信号量， 当没产品时阻止消费者消费
-pthread_mutex_t mutex; // 互斥信号量， 一次只有一个线程访问缓冲
-
+// pthread_mutex_t mutex; // 互斥信号量， 一次只有一个线程访问缓冲
+sem_t mutex;
 int product_id = 0;   //生产者id
 int consumer_id = 0; //消费者id
 
@@ -42,12 +43,13 @@ void *product()
    while(1)
    {
       // 用sleep的数量可以调节生产和消费的速度，便于观察
-      sleep(1);
+      // sleep(1);
       // printf("----- product%d  before sem_wait(&empty_sem)  -------\n",id);
       sem_wait(&empty_sem);
       // printf("----- product%d  after  sem_wait(&empty_sem) -------\n",id);
       // printf("----- product%d  before  sem_wait(&mutex) -------\n",id);
-      pthread_mutex_lock(&mutex);
+      // pthread_mutex_lock(&mutex);
+      sem_wait(&mutex);
       // printf("----- product%d  after sem_wait(&mutex)  -------\n",id);
      
       in = in % M;
@@ -57,7 +59,8 @@ void *product()
       print();  
       ++in;
      
-      pthread_mutex_unlock(&mutex);
+      // pthread_mutex_unlock(&mutex);
+      sem_post(&mutex);  
       sem_post(&full_sem);  
    }
 }
@@ -69,20 +72,21 @@ void *consumer()
    while(1)
    {
       // 用sleep的数量可以调节生产和消费的速度，便于观察
-      sleep(1);
+      // sleep(1);
       //sleep(1);
      
       sem_wait(&full_sem);
-      pthread_mutex_lock(&mutex);
-     
+      // pthread_mutex_lock(&mutex);
+      sem_wait(&mutex);
+   
       out = out % M;
       printf("consumer%d in position %d. like: \t", id, out);
      
       buff[out] = 0;
       print();
       ++out;
-     
-      pthread_mutex_unlock(&mutex);
+      sem_post(&mutex);  
+      // pthread_mutex_unlock(&mutex);
       sem_post(&empty_sem);
    }
 }
@@ -97,16 +101,18 @@ int main()
    // 初始化同步信号量
    int ini1 = sem_init(&empty_sem, 0, M); 
    int ini2 = sem_init(&full_sem, 0, 0);  
-   if(ini1 && ini2 != 0)
+   int ini3 = sem_init(&mutex,0,1);
+   //初始化互斥信号量 
+   // int ini3 = pthread_mutex_init(&mutex, NULL);
+   // if(ini3 != 0)
+   // {
+   //    printf("mutex init failed \n");
+   //    exit(1);
+   // } 
+
+   if(ini1 && ini2&& ini3  != 0)
    {
       printf("sem init failed \n");
-      exit(1);
-   } 
-   //初始化互斥信号量 
-   int ini3 = pthread_mutex_init(&mutex, NULL);
-   if(ini3 != 0)
-   {
-      printf("mutex init failed \n");
       exit(1);
    } 
    // 创建N个生产者线程
